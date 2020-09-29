2b61a72a361846c4d58a7936e25bfa49dc9a5b4c
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-09-29 18:26:48 +0400

    feat:implements 2 methods reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    /* 1 способ
+        return +((''+Math.abs(n)).split('').reverse().join(''));
+    */
+
+    /* 2 Способ */
+    
+    let res = 0;
+    let tmp = Math.abs(n);
+    while (tmp){
+        res = res * 10 + tmp % 10;
+        tmp = Math.floor(tmp / 10);
+    }
+    return res;
 }
